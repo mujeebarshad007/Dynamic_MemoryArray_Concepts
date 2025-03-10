470eca0903189e3b6b76fbfa251be50657ae1af6
mujeebarshad007 <mujeeb.arshad001@gmail.com>
2025-03-11 01:17:15 +0500

    Initialized separate repository

--- 1.cpp
+++ 1.cpp
@@ -1,24 +1,24 @@
 #include <iostream>
-#include <climits>
-
 using namespace std;
 int main()
 {
-    int arr[5] = {1, 2, 3, 4, 5};
-    int max1 = INT_MIN;
-    int max2 = INT_MIN;
-    for (int i = 0; i < 5; i++)
+    int arr[2][3] = {{1, 2, 3}, {4, 5, 6}};
+    int arr1[3][2] = {{7, 8}, {9, 10}, {11, 12}};
+    int list[2][2] = {0};
+    for (int i = 0; i < 2; i++)
     {
-        if (arr[i] > max1)
+        for (int j = 0; j < 2; j++)
         {
-            max2 = max1;
-            max1 = arr[i];
-        }
-        else if (arr[i] > max2 && max2 != max1)
-        {
-            max2 = arr[i];
+            list[i][j] += arr[i][j] * arr1[j][i];
         }
     }
-    cout << max2 << endl;
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            cout << list[i][j] << " ";
+        }
+        cout << endl;
+    }
     return 0;
 }
