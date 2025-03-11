686b72ac0ede2e688d6aae1a60282baefa94285d
mujeebarshad007 <mujeeb.arshad001@gmail.com>
2025-03-11 23:41:53 +0500

    DSA

--- 1.cpp
+++ 1.cpp
@@ -2,23 +2,7 @@
 using namespace std;
 int main()
 {
-    int arr[2][3] = {{1, 2, 3}, {4, 5, 6}};
-    int arr1[3][2] = {{7, 8}, {9, 10}, {11, 12}};
-    int list[2][2] = {0};
-    for (int i = 0; i < 2; i++)
-    {
-        for (int j = 0; j < 2; j++)
-        {
-            list[i][j] += arr[i][j] * arr1[j][i];
-        }
-    }
-    for (int i = 0; i < 2; i++)
-    {
-        for (int j = 0; j < 2; j++)
-        {
-            cout << list[i][j] << " ";
-        }
-        cout << endl;
-    }
+    int a;
+    a = 1234;
     return 0;
 }
